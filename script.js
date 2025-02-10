d8a399d8c477ce6d65609f92b0c24deaa3d13aa7
Lucas <lucasnpagella@outlook.com.ar>
2025-02-10 03:55:26 -0300

    2nd commit

---
+++ script.js
@@ -0,0 +1,222 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".about__section__title", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about__section__title",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".about1", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about1",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".about2", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about2",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".about3", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about3",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".about4", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about4",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".about5", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about5",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".about6", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about6",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".about7", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".about7",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".skills__section__title", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".skills__section__title",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+
+
+gsap.to(".js__container", {
+    opacity: 1,
+    x: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".js__container",
+        start: "top 68%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".html__container", {
+    opacity: 1,
+    x: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".html__container",
+        start: "top 68%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".css__container", {
+    opacity: 1,
+    x: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".css__container",
+        start: "top 68%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".responsive-design__container", {
+    opacity: 1,
+    x: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".responsive-design__container",
+        start: "top 68%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+
+
+gsap.to(".projects__section__title", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".projects__section__title",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+
+gsap.to(".project__container", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".project__container",
+        start: "top 75%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".project__container2", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".project__container2",
+        start: "top 75%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".contact__section__h2", {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".contact__section__h2",
+        start: "top 90%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+gsap.to(".contact__section__form", {
+    opacity: 1,
+    scale: 1,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".contact__section__form",
+        start: "top 70%",
+        toggleActions: "play reverse play reverse"
+    }
+});
